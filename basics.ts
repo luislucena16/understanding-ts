cb2670c02a304f293b2b00a05ab1ac1a93a34495
Luis Lucena <llucena@shokworks.io>
2021-12-05 13:58:02 -0400

    use between TypeScript and JavaScript

---
+++ basics.ts
@@ -0,0 +1,20 @@
+function add(n1: number, n2: number, printResult: boolean, resultPhrase: string) {
+    /* if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorrect input!');
+    } */
+
+    const result = n1 + n2;
+    if (printResult === true) {
+        console.log(resultPhrase + result);
+    } else {
+        return result;
+    }
+}
+
+const number1 = 5; // 5.0
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+add(number1, number2, printResult, resultPhrase);
+ 
